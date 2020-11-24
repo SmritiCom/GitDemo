6d1ceb122f45a195fa18a6819e5653a37aa73999
smriti <smriti.dalal@comviva.com>
2020-11-24 16:17:23 +0530

    eight commit

---
+++ src/test/java/com/mycompany/app/testclass.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+public class testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New class");
+
+	}
+
+}
